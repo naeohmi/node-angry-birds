fa6fab7cd20f32a3a353e7abd1d07a402e4a6466
naeohmi <naeohmi@gmail.com>
2017-04-23 15:04:50 -0400

    yay linked them up

--- angrybirds/routes/index.js
+++ angrybirds/routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var birds = require('../data/birds');
+
 var router = express.Router();
 
 //`/` should render a home page with a link to '/angry-birds'
@@ -8,22 +10,23 @@ router.get('/', function(req, res, next) {
     });
 });
 
-//`/angry-birds` will render a view of ALL angry birds
-//has each bird's name, ability, image, and group visible on the page
-//has a link to `/angry-birds/BIRDS_ID`
-//There should also be a link back to `/`
+// `/angry-birds` will render a view of ALL angry birds
+// has each bird's name, ability, image, and group visible on the page
+// has a link to `/angry-birds/BIRDS_ID`
+// There should also be a link back to `/`
 router.get('/angry-birds', function(req, res, next) {
-	res.render('users', { //maybe change to index (need to decide one file or multiple)
-		title: 'angry-birds'
+	res.render('users', {
+		title: 'angry-birds',
+		birds: birds
 	});
 });
 
-//`/angry-birds/:id` will render a view of JUST the angry bird with the matching ID. This should have the bird's name, ability, image, and group visible on the page
-//also has links back to `/` and `/angry-birds`
-router.get('/angry-birds:id', function(req, res, next) {
-	res.render('users', {
-		title: 'angry-birds:id'
-	});
-});
+// //`/angry-birds/:id` will render a view of JUST the angry bird with the matching ID. This should have the bird's name, ability, image, and group visible on the page
+// //also has links back to `/` and `/angry-birds`
+// router.get('/angry-birds:id', function(req, res, next) {
+// 	res.render('users', {
+// 		title: 'angry-birds:id'
+// 	});
+// });
 
 module.exports = router;
--- angrybirds/routes/users.js
+++ angrybirds/routes/users.js
@@ -1,11 +1,26 @@
 var express = require('express');
+var birds = require('../data/birds');
+// console.log('==>>', birds);
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/birds', function(req, res, next) {
-  res.render('birds', {
-  	title: 'angryyy'
+router.get('/angry-birds', function(req, res, next) {
+  res.render('users', {
+  	title: 'angryyy',
+  	birds: birds
   });
 });
+
+
+//`/angry-birds` will render a view of ALL angry birds
+//has each bird's name, ability, image, and group visible on the page
+//has a link to `/angry-birds/BIRDS_ID`
+//There should also be a link back to `/`
+// router.get('/angry-birds', function(req, res, next) {
+// 	res.render('users', { //maybe change to index (need to decide one file or multiple)
+// 		title: 'angry-birds',
+// 		birds: birds
+// 	});
+// });
 
 module.exports = router;
